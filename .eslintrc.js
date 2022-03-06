65b412b28410e0b6f11f7f2fafe5c722024d6af7
Farshatov Alex <fla19991510@gmail.com>
2022-03-07 00:01:03 +0700

    added env file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,41 @@
+const path = require('path');
+
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier", "import", "react", "react-hooks"],
   rules: {
+    quotes: [2, "single"],
+    "prettier/prettier": ["error", { singleQuote: true }],
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    "no-plusplus": "off",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_", "ignoreRestSiblings": true }],
     "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "react/button-has-type": "off",
+    "react/no-array-index-key": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+    "jsx-a11y/alt-text": "off",
+    "jsx-a11y/no-noninteractive-element-interactions": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-autofocus": "off",
+    "jsx-a11y/anchor-is-valid": "off",
     "import/prefer-default-export": "off",
     "import/no-extraneous-dependencies": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "no-underscore-dangle": "off",
-    "@typescript-eslint/naming-convention": "off",
     "import/order": [
-      'error',
+      "error",
       {
-        groups: [['builtin', 'external'], 'internal', ['sibling', 'parent', 'index']],
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          ["sibling", "parent", "index"],
+        ],
         pathGroups: [
           {
             pattern: "@reduxjs/**",
@@ -22,20 +46,17 @@ module.exports = {
             group: "external",
           },
         ],
-        "pathGroupsExcludedImportTypes": ["external"],
-        'newlines-between': 'always',
-        warnOnUnassignedImports: true
-      }
+        pathGroupsExcludedImportTypes: ["external"],
+        "newlines-between": "always",
+        warnOnUnassignedImports: true,
+      },
     ],
     "no-param-reassign": "off",
-    "prettier/prettier": [
-      "error",
-      { singleQuote: true }
-    ],
-    quotes: [2, "single"]
+    "prettier/prettier": ["error", { singleQuote: true }],
+    quotes: [2, "single"],
   },
   settings: {
-    "import/internal-regex": "^@.*"
+    "import/internal-regex": "^@.*",
   },
   extends: [
     "eslint:recommended",
@@ -47,6 +68,7 @@ module.exports = {
     "prettier",
   ],
   parserOptions: {
-    project: './tsconfig.json'
+    project: path.resolve(__dirname, "tsconfig.json"),
+    sourceType: "module",
   },
 };
